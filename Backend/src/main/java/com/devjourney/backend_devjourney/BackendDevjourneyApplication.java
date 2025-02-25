ec83038cc8e7985c725b76419603962610cc2987
Joao Pedro <joaopedro251900@gmail.com>
2025-02-25 20:54:03 -0300

    BackendJava

---
+++ Backend/src/main/java/com/devjourney/backend_devjourney/BackendDevjourneyApplication.java
@@ -0,0 +1,13 @@
+package com.devjourney.backend_devjourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDevjourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDevjourneyApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/devjourney/backend_devjourney/BackendDevjourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjourney.backend_devjourney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDevjourneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
